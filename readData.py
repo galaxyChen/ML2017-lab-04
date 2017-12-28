e24452ccc8ec978dd40a5a6cc5ec7a53ef888e67
galaxyChen <549278303@qq.com>
2017-12-28 15:39:03 +0800

    fix bug

--- readData.py
+++ readData.py
@@ -6,7 +6,7 @@ import numpy as np
 
 data = pd.read_table('./ml_100k/u1.base',names=['user_id','item_id','rating','timestamp'])[['user_id','item_id','rating']]
 data2 = pd.read_table('./ml_100k/u1.test',names=['user_id','item_id','rating','timestamp'])[['user_id','item_id','rating']]
-train_data = data.pivot_table(values='rating',index='user_id',columns='item_id')
+train_data = data.pivot_table(values='rating',index='item_id',columns='user_id')
 train_r_data = (~train_data.isnull()).astype(int)
 train_set = np.matrix(train_data.fillna(0).as_matrix())
 train_r = np.matrix(train_r_data.as_matrix())
@@ -20,16 +20,16 @@ data2 = data2[t]
 
 test_data = train_data.to_dict()
 test_r = train_r_data.to_dict()
-for item in test_data:
-    for user in test_data[item]:
-        test_data[item][user]=0
-        test_r[item][user]=0
+for user in test_data:
+    for item in test_data[user]:
+        test_data[user][item]=0
+        test_r[user][item]=0
         
         
 for i in range(len(data2)):
     row = data2.iloc[i,:]
-    test_data[row['item_id']][row['user_id']] = row['rating']
-    test_r[row['item_id']][row['user_id']] = 1
+    test_data[row['user_id']][row['item_id']] = row['rating']
+    test_r[row['user_id']][row['item_id']] = 1
 
 test_data = pd.DataFrame(test_data)
 test_r = pd.DataFrame(test_r)
